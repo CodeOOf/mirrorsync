1c9306f634259164b676252d02e8b8558607515c
Viktor H. Ingre <viktor.ingre@codeoof.com>
2024-12-07 10:48:44 +0000

    Start defining startup checks in main script

--- opt/mirrorsync/mirrorsync.sh
+++ opt/mirrorsync/mirrorsync.sh
@@ -1,1 +1,22 @@
 #!/bin/env bash
+# 
+# Mirrorsync
+# By: Viktor H. Ingre <viktor.ingre@codeoof.com>
+#
+# Latest version available at:
+# https://github.com/CodeOOf/mirrorsync
+#
+# Copyright (c) 2024 CodeOOf
+
+CONFIG_FILE="/etc/mirrorsync/mirrorsync.conf"
+REPO_CONFIG_DIR="/etc/mirrorsync/repos.conf.d"
+
+# Verify config file is readable
+if [ ! -r "$CONFIG_FILE" ]; then
+    echo "$CONFIG_FILE is not availble or readable, exiting..."
+    exit 1
+fi
+
+# Check if current path is writable
+
+# Read settings
