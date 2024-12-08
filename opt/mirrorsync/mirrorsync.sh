74d3fd0fb312824f70e7277eb1e0b5e29a613add
Viktor H. Ingre <viktor.ingre@codeoof.com>
2024-12-08 19:37:38 +0000

    Implemented the first draft of the script

--- opt/mirrorsync/mirrorsync.sh
+++ opt/mirrorsync/mirrorsync.sh
@@ -61,8 +61,6 @@ if [ -z "$logfile" ]; then
     logfile="$0.log"
 fi
 
-if [ -z "$rsynclog_prefix" ]; then rsynclog_prefix="rsync"; fi
-
 if [ -z "$dstpath" ]; then 
     echo "Missing config value \"dstpath\" that is required to know where to write the mirror data, exiting..."
     exit 1
@@ -79,16 +77,16 @@ printf '%s\n' "$$" > "$LOCKFILE"
 
 # Check for existing lockfile to avoid multiple simultaneously running syncs
 # If lockfile exists but process is dead continue anyway
-if [ -e "$lockfile" ] && ! kill -0 "$(< "$lockfile")" 2>/dev/null; then
-        printf "[%(%F %T)T] Warning: lockfile exists but process dead, continuing...\n" -1 >> "${logpath}/$logpath" 2>&1
-        rm -f "$lockfile"
+if [ -e "$LOCKFILE" ] && ! kill -0 "$(< "$LOCKFILE")" 2>/dev/null; then
+        printf "[%(%F %T)T] Warning: lockfile exists but process dead, continuing...\n" -1 >> "$LOGFILE" 2>&1
+        rm -f "$LOCKFILE"
 elif [ -e "$LOCKFILE" ]; then
-        printf "[%(%F %T)T] Update already in progress...\n" -1 >> "${logpath}/$logpath" 2>&1
+        printf "[%(%F %T)T] Update already in progress...\n" -1 >> "$LOGFILE" 2>&1
         exit 1
 fi
 
 # Start updating each mirror repo
-printf "[%(%F %T)T] Started updating repos...\n" -1 >> "${logpath}/$logpath" 2>&1
+printf "[%(%F %T)T] Started updating repos...\n" -1 >> "$LOGFILE" 2>&1
 for file in "$REPOCONFIGS"
 do
     localpath=""
@@ -114,6 +112,7 @@ do
         printf "[%(%F %T)T] Error: The path \"$s\" is not writable, cannot update this mirror\n" -1 \
         "$dst" >> "${logpath}/$logpath" 2>&1
     fi
+    LOGFILE="$LOGFILE"
     
     # Verify network connectivity against the remote and the select first available
     while [ "$remote" in "$remotes"]
@@ -131,7 +130,7 @@ do
                 ;;
             *)
                 printf "[%(%F %T)T] Error: The remote path \"$s\" contains invalid protocol\n" -1 "$remote" >> \
-                "${logpath}/$logpath" 2>&1
+                "$LOGFILE" 2>&1
                 ;;
         esac
         
@@ -143,14 +142,14 @@ do
 
         # If we get here the connection did not work
         printf "[%(%F %T)T] Warning: No connection with \"$s\", continuing with next...\n" -1 "$remote" >> \
-        "${logpath}/$logpath" 2>&1
+        "$LOGFILE" 2>&1
 
     done
 
     # If no source url is defined it means we did not find a valid remote url that we can connect to now
     if [ -z "$src" ]; then
         printf "[%(%F %T)T] Error: No connection with any source provided in\"$s\", cannot update this mirror\n" -1 \
-        "$file" >> "${logpath}/$logpath" 2>&1
+        "$file" >> "$LOGFILE" 2>&1
         break
     fi
 
@@ -158,33 +157,81 @@ do
     # So we start with that
     checkresult=""
     if [ -z "$filelistfile" ]; then
-        printf "[%(%F %T)T] Info: No filelistfile is provided in \"$s\"\n" -1 "$file" >> "${logpath}/$logpath" 2>&1
+        printf "[%(%F %T)T] Info: No filelistfile is provided in \"$s\"\n" -1 "$file" >> "$LOGFILE" 2>&1
     elif [ "$PORT" == "$RSYNC_PORT" ]
         checkresult=$(rsync --no-motd --dry-run --out-format="%n" "${src}/$filelistfile" "${dst}/$filelistfile")
     else
-        printf "[%(%F %T)T] Warning: This protocol used with \"$s\" has not yet been implemented, cannot update this 
-        mirror now. Move a implemented protocol higher up in priority list of remote sources if there are any to solve 
-        this at the moment\n" -1 "$src" >> "${logpath}/$logpath" 2>&1
+        printf "[%(%F %T)T] Warning: The protocol used with \"$s\" has not yet been implemented. Move another protocol 
+        higher up in list of remote sources if there are any to solve this at the moment. Until then this script cannot 
+        update the mirror\n" -1 "$src" >> "$LOGFILE" 2>&1
     fi
 
     # Check the results of the filelist against the local
     if [ -z "$checkresult" ] && [ ! -z "$filelistfile"]; then
-        printf "[%(%F %T)T] Info: Filelistfile is unchanged in \"$s\" with remote, will not update mirror\n" -1 \
-        "$dst" >> "${logpath}/$logpath" 2>&1
+        printf "[%(%F %T)T] Info: Filelistfile is unchanged at \"$s\", will not update mirror\n" -1 \
+        "$src" >> "$LOGFILE" 2>&1
         break
     fi
+
+    # Create a new exlude file before running the update
+    EXCLUDEFILE="${PWD}/${localpath}_exclude.txt"
+    # Clear file
+    > $EXCLUDEFILE
+
+    # Validate the excludelist variable is a array
+    if [ ! "${declare -p excludelist}" =~ "declare -a"]; then
+        printf "[%(%F %T)T] Error: The exclude list defined for \"$s\" is invalid, will not parse it." -1 \
+        "$localpath" >> "$LOGFILE" 2>&1
+    fi
+
+    # Generate the version exclude list, assumes that the versions are organized at root
+    if [ "$minMajor" > 0 ]; then
+        for i in $(seq 0 $((minMajor -1)))
+        do
+            excludelist+=("/$i" "$i.*")
+        done
+            excludelist+=("/$minMajor")
+        if [ "$minMinor" > 0 ]; then
+            for i in $(seq 0 $((minMinor -1)))
+            do
+                excludelist+=("/$minMajor.$i")
+            done
+        fi
+    fi
+
+    # Write the new excludes into the excludefile
+    for i in "${excludelist[@]}"
+    do
+        printf "$i\n" >> $EXCLUDEFILE
+    done
 
     # Depending on what protocol the url has the approch on syncronizing the repo is different
     case "$port" in
         "$RSYNC_PORT")
+            OPTS=(-vrlptDSH --delete-excluded --delete-delay --delay-updates --exclude-from=$EXCLUDEFILE)
+            UPDATELOGFILE="${logpath}/$(date +%y%m%d%H%M)_${localpath}_rsyncupdate.log"
+
+            # Header for the new log fil
+            printf "Using the following arguments for this run:\n[" >> "$UPDATELOGFILE" 2>&1
+            printf "%s " "${OPTS[*]}" >> "$UPDATELOGFILE" 2>&1
+            printf "]\n\n---\n" >> "$UPDATELOGFILE" 2>&1
+
+            # Start updating
+            rsync "${opts[@]}" "${src}/" "${dst}/" >> "$UPDATELOGFILE" 2>&1
+
+            # Finished
+            printf "[%(%F %T)T] Finished updating local repo: ${S}.\n" -1 "$localpath" >> "$LOGFILE" 2>&1
             ;;
         *)
-            printf "[%(%F %T)T] Error: This protocol used with \"$s\" has not yet been implemented, cannot update this 
-            mirror now. Move a implemented protocol higher up in priority list of remote sources if there are any to 
-            solve this at the moment\n" -1 "$src" >> "${logpath}/$logpath" 2>&1
+            printf "[%(%F %T)T] Warning: The protocol used with \"$s\" has not yet been implemented. Move another 
+            protocol higher up in list of remote sources if there are any to solve this at the moment. Until then this 
+            script cannot update the mirror\n" -1 "$src" >> "$LOGFILE" 2>&1
             ;;
     esac
-
 done
-exit 0
 
+# Finished
+printf "[%(%F %T)T] Finished, everything possible is now up to date.\n" -1 >> "$LOGFILE" 2>&1
+rm -f "$LOCKFILE"
+
+exit 0
