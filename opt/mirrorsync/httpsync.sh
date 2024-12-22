05f03fb9fa55e169f99afab4225ffde073d9bc9c
Viktor H. Ingre <viktor.ingre@codeoof.com>
2024-12-22 17:43:42 +0000

    Added more logic to httpsync

--- opt/mirrorsync/httpsync.sh
+++ opt/mirrorsync/httpsync.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 # 
-# Mirrorsync - httpsync.sh
+# Mirrorsync - httpssynclist.sh
 # Author: Viktor H. Ingre <viktor.ingre@codeoof.com>
 #
 # Latest version available at:
@@ -11,20 +11,18 @@
 # Global Variables
 DEBUG_ARG=0
 DELETE_AFTER=0
-DELETE_BEFORE=0
-DELETE_DURING=0
 DELETE_EXCLUDE=0
 EXCLUDES=()
 EXCLUDE_FILE=""
 HUMAN_READABLE=0
 LIST_ONLY=0
-NOPARENT_ARG=0
 PROGRESS_ARG=0
 RECURSIVE_ARG=0
 STATS=0
 VERBOSE_ARG=0
 POSITIONAL_ARGS=()
-TRANSFER_SIZE=0
+SRC=""
+DST=""
 
 # Log functions
 log() { printf "[%(%F %T)T] %s\n" -1 "$*" >&2; }
@@ -50,13 +48,8 @@ Arguments:
     Activate Debug Mode, provides a very detailed output to the system console.
 
   --delete-after
-    Files are transferd to tmp and deleted at destination after the transfer is complete
-
-  --delete-before
-    Files at destination are deleted before transfering the files
-
-  --delete-during
-    Files at destination are deleted prior to transfering the new file
+    Files are transferd to "/tmp" and deleted at destination after the transfer is complete.
+    Default is deletion prior to transfer.
  
   --delete-excluded
     Delete files and directoris from destination that are excluded or not found at source
@@ -72,9 +65,6 @@ Arguments:
 
   -l, --list-only
     Only outputs what files that would change
-
-  -np, --no-parent
-    Tries to recognize parent links and ignores them from ascending to the parent directory
 
   --progress
     Outputs the synchronization progress
@@ -98,15 +88,12 @@ while [ "$#" -gt 0 ]; do
         --*'='*) shift; set -- "${arg%%=*}" "${arg#*=}" "$@"; continue;;
         -d|--debug) DEBUG_ARG=1;;
         --delete-after) DELETE_AFTER=1;;
-        --delete-before) DELETE_BEFORE=1;;
-        --delete-during) DELETE_DURING=1;;
         --delete-exclude) DELETE_EXCLUDE=1;;
         --exclude) EXCLUDES=($2); shift;;
         --exclude-file) EXCLUDE_FILE="$2"; shift;;
         -h|--help) usage; exit 0;;
         -hr|--human-readable) HUMAN_READABLE=1;;
         -l|--list-only) LIST_ONLY=1;;
-        -np|--no-parent) NOPARENT_ARG=1;;
         --progress) PROGRESS_ARG=1;
         -r|--recursive) RECURSIVE_ARG=1;;
         --stats) STATS=1;;
@@ -120,6 +107,13 @@ while [ "$#" -gt 0 ]; do
     esac
     shift || error_argument "Option '${arg}' requires a value"
 done
+
+# Populate the source and destination
+SRC="${POSITIONAL_ARGS[0]}"
+DST="${POSITIONAL_ARGS[1]}"
+
+# If the destination path is empty then do from user position
+if [ -z "$DST" ]; then DST=$(pwd); fi
 
 # Function to validate if value is matched in a array of queries
 # Usage: arraymatch "value_to_test" "array of values to validate"
@@ -142,10 +136,10 @@ arraymatch() {
 
 # This is a recursive function that will parse through a website with listed items and compare with local
 # returning a list of itemes out of sync
-# Usage: httpsync "http://example.com/pub/repo/" "/my/local/destination/" "(EXCLUDE/,*FILES,and~,/dirs)"
+# Usage: httpssynclist "http://example.com/pub/repo/" "/my/local/destination/" "(EXCLUDE/,*FILES,and~,/dirs)"
 # With the ending slash on paths and urls
 # excludes starting with "/" only excludes from root
-httpsync() {
+httpssynclist() {
     local filelist=()
     local baseurl=$1
     local localpath=$2
@@ -191,12 +185,14 @@ httpsync() {
             done
 
             # Call recursivly until no more directories are found
-            local recursivecall=$(httpsync "$url" "$dst" "${querylist[*]}" | tr -d '\0')
+            if [ $RECURSIVE_ARG -eq 1 ]; then
+                local recursivecall=$(httpssynclist "$url" "$dst" "${querylist[*]}" | tr -d '\0')
 
-            # Only add to collection if array is populated
-            local is_array=$(declare -p recursivecall | grep '^declare -a')
-            if [ -z "$is_array" ]; then
-                filelist+=$recursivecall
+                # Only add to collection if array is populated
+                local is_array=$(declare -p recursivecall | grep '^declare -a')
+                if [ -z "$is_array" ]; then
+                    filelist+=$recursivecall
+                fi
             fi
         # As long as it is not ending slash, assume as file
         elif [ "${href: -1:1}" != $'/' ]; then
@@ -238,7 +234,7 @@ httpsync() {
 
                             # Check if file is changed based on date if date was extracted
                             if [ ! -z $modified ] && [ $modified > $modified_local ]; then
-                                debug "Local file \"${dst}\" is unchanged at remote based on date"
+                                info "Local file \"${dst}\" is unchanged at remote based on date"
 
                                 # Continue with the next item in the loop above this inner as the file is unchanged
                                 continue 2
@@ -246,14 +242,14 @@ httpsync() {
 
                             # If we cannot test by date, test with size
                             if [ $bytes -eq $bytes_local ]; then 
-                                debug "Local file \"${dst}\" is unchanged at remote based on size"
+                                info "Local file \"${dst}\" is unchanged at remote based on size"
 
                                 # Continue with the next item in the loop above this inner as the file is unchanged
                                 continue 2
                             fi
 
                             # Break this first loop as we found the file
-                            debug "The remote file \"${url}\" has changed from local \"${dst}\""
+                            info "The remote file \"${url}\" has changed from local \"${dst}\""
                             break
                         fi
                     done
@@ -262,16 +258,8 @@ httpsync() {
                     debug "Added a file of size ${filesize}B from \"${url}\" to the list, it was last modifed 
 \"${modified}\""
                     # Add to the array
-                    filentry=("ADD" "$url" "$dst" "$bytes")
+                    filentry=("$url" "$dst" "$bytes" "$href")
                     filelist+=($filentry)
-
-                    # Find this file in localfiles and remove it
-                    for index in "${!localfiles[@]}"
-                    do
-                        if [ "${localfiles[$index]}" == "${dst}" ]; then
-                            unset localfiles[$index]
-                        fi
-                    done
                 else
                     debug "Not a file \"$url\", ignoring path"
                 fi
@@ -283,14 +271,80 @@ httpsync() {
         fi
     done
 
-    # Add the remaining local files for deletion
-    for localfile in "${localfiles[@]}"
-    do
-        # Add to the array
-        filentry=("DELETE" "" "$localfile" "")
-        filelist+=($filentry)
-    done
+    if [ $DELETE_EXCLUDE -eq 1 ]; then
+        # Add the remaining local files for deletion
+        for localfile in "${localfiles[@]}"
+        do
+            # Add to the array
+            filentry=("" "$localfile" "" "")
+            filelist+=($filentry)
+        done
+    fi
 
     # Return array
     echo "${filelist[*]}"
 }
+
+# Read all the exclude patterns from the file
+if [ ! -z "$EXCLUDE_FILE" ]; then
+    IFS=$'\n' read -d '' -r -a EXCLUDES < $EXCLUDE_FILE
+fi
+
+# If the source and destination is not ending with a slash, add it
+if [ "${SRC:-1:1}" != "/" ]; then SRC="${SRC}/"; debug "Added a \"/\" to the source: $SRC"; fi
+if [ "${DST:-1:1}" != "/" ]; then DST="${DST}/"; debug "Added a \"/\" to the destination: $DST"; fi
+
+RESULTS=$(httpssynclist "$SRC" "$DST" "${EXCLUDES[*]}")
+
+# If we only suppose to print the transfer size
+if [ $STATS -eq 1 ]; then
+    transfer_size=0
+    for fileinfo in "${RESULTS[@]}"
+    do
+        if [ ! -z "${fileinfo[0]}" ]; then transfer_size+=$fileinfo[2]; fi
+    done
+
+    # Convert the output to human readable numbers
+    if [ $HUMAN_READABLE -eq 1]; then transfer_size=$(echo $transfer_size | numfmt --to=iec-i); fi
+
+    printf "%s" "$transfer_size" >%1
+    exit 0
+fi
+
+# If we only suppose to print the list
+if [ $LIST_ONLY -eq 1 ]; then
+    for fileinfo in "${RESULTS[@]}"
+    do
+        if [ ! -z "${fileinfo[0]}" ]; then 
+            progress "*NEW* ${FILE[1]}"
+        else
+            progress "Remove ${fileinfo[1]}"
+        fi
+    done
+
+    info "Done"
+    exit 0
+fi
+
+# Main Sync
+for fileinfo in "${RESULTS[@]}"
+do
+    if [ ! -z "${fileinfo[0]}" ]; then 
+        progress "Transfering ${fileinfo[1]}"
+        if [ $DELETE_AFTER -eq 1 ]; then
+            tmpfile="/tmp/${fileinfo[3]}"
+            curl "${fileinfo[0]}" --output "$tmpfile" 2>&1
+            rm "${fileinfo[1]}" 2>&1
+            mv "$tmpfile" "${fileinfo[1]}" 2>&1
+        else
+            rm "${fileinfo[1]}" 2>&1
+            curl "${fileinfo[0]}" --output "${fileinfo[1]}" 2>&1
+        fi
+    else
+        progress "Removing ${fileinfo[1]}"
+        rm -r "${fileinfo[1]}"
+    fi
+done
+
+info "Done"
+exit 0
